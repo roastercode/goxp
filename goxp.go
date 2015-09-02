853de7479f5e9676a06cbcfe2721678bbc1c4260
4ur3l13n <aurelien@hackers.camp>
2015-09-02 11:13:10 +0200

    goxp top level application

---
+++ goxp.go
@@ -0,0 +1,39 @@
+/*
+
+Package GoXp is a powerful package for quickly writing modular web application/services in Golang
+independantly but fully inspired from the martini package.
+
+Full guide http://github.com/4ur3l13n/goxp
+
+The idea is also at the end to write a version that use booster from 4ur3l13n and not injector
+from github.com/codegangsta/inject
+
+*/
+
+package goxp
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"reflect"
+
+	"github.com/codegangsta/inject"
+)
+
+// GoXp represents the top level web application, inject.Injector methods can be invoked to map services on global level.
+type Goxp struct {
+	inject.Injector
+	handlers []Handler
+	action   Handler
+	logger   *log.Logger
+}
+
+// New creates a bare bones Goxp instance. Use this method if you want to have full control over the middleware that is used.
+func New() *Goxp {
+	m := &Goxp{Injector: inject.New(), action: func() {}, logger: log.New(os.Stdout, "[goxp] ", 0)}
+	m.Map(m.logger)
+	m.Map(defaultReturnHandler())
+	return m
+}
+
