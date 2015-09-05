903ca313f8f6d78b46dfe21f46d7a83578eff37e
4ur3l13n <aurelien@hackers.camp>
2015-09-05 18:38:16 +0200

    recovery.go +First commit

---
+++ recovery.go
@@ -0,0 +1,19 @@
+/*
+
+TODO modify the code with github.com/4ur3l13n/boot
+
+*/
+
+package goxp
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"runtime"
+
+	"github.com/codegansta/inject"
+)
+
