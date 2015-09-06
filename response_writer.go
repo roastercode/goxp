809da70897ca882792c7fb2341e31bc8f3f652ae
4ur3l13n <aurelien@hackers.camp>
2015-09-06 14:48:51 +0200

    response_writer.go +first commit

--- response_writer.go
+++ response_writer.go
@@ -6,3 +6,22 @@ import (
 	"net"
 	"net/http"
 )
+
+// ResponseWriter is a wrapper around http.ResponseWriter that provides extra information about
+// the response. It is recommended that middleware handlers use this construct to wrap a responsewriter
+// if the functionality calls for it.
+type ResponseWriter interface {
+	http.ResponseWriter
+	http.Flusher
+	http.Hijacker
+	// Status returns the status code of the response or 0 if the response has not been Written
+	Status() int
+	// Written returns whether or not the ResponseWriter has been written.
+	Written() bool
+	// Size returns the size of the response body.
+	Size() int
+	// Before allows for a function to be called before the ResponseWriter has been written to. This is
+	// useful for setting headers or any other operations tat must happe before a response has been written.
+	Before(BeforeFunc)
+}
+
