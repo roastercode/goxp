08d9de2669cad0659dfaf08713d4f1be3b32bac2
4ur3l13n <aurelien@hackers.camp>
2015-09-09 19:38:25 +0200

    response_writer_test.go +func

--- response_writer_test.go
+++ response_writer_test.go
@@ -38,3 +38,11 @@ func newHijackableResponse() *hijackableResponse {
 	return &hijackableResponse{}
 }
 
+func (h *hijackableResponse) Header() http.Header           { return nil }
+func (h *hijackableResponse) Write(buf []byte) (int, error) { return 0, nil }
+func (h *hijackableResponse) WriteHeader(code int)          {}
+func (h *hijackableResponse) Flush()                        {}
+func (h *hijackableResponse) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	h.Hijacked = true
+	return nil, nil, nil
+}
