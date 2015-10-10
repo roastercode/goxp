21318290c1dbd3b1b910cde8629370d8794efc2d
Aurélien Desbrières <aurelien@hackers.camp>
2015-10-10 18:28:48 +0200

    router_test.go + req5

--- router_test.go
+++ router_test.go
@@ -14,9 +14,16 @@ recorder := httptest.NewRecorder()
 req, _ := http.NewRequest("GET", "http://localhost:3000/foo", nil)
 context := New().createContext(recorder, req)
 
-req2, _ := http.newRequest("POST", "http://localhost:3000/bar/bat", nil)
+req2, _ := http.NewRequest("POST", "http://localhost:3000/bar/bat", nil)
 context2 := New().createContext(recorder, req2)
 
-req3, _ := http:New.Request("DELETE", "http://localhost:3000/baz", nil)
+req3, _ := http.NewRequest("DELETE", "http://localhost:3000/baz", nil)
 context3 := New().createContext(recorder, req3)
 
+req4, _ := http.NewRequest("PATCH", "http://localhost:3000/foo", nil)
+context4 := New().createContext(recorder, req4)
+
+req5, _ := http.NewRequest("GET", "http://localhost:3000/theory/and/practice", nil)
+context5 := New().createContext(recorder, req5)
+
+}
