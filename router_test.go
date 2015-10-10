1723299bede35c64bf8826773e9b11731a5b4016
Aurélien Desbrières <aurelien@hackers.camp>
2015-10-10 18:03:13 +0200

    router_test.go + req3

--- router_test.go
+++ router_test.go
@@ -1,1 +1,22 @@
 package goxp
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func Test_Routine(t *testing.T) {
+router := NewRouter()
+recorder := httptest.NewRecorder()
+
+req, _ := http.NewRequest("GET", "http://localhost:3000/foo", nil)
+context := New().createContext(recorder, req)
+
+req2, _ := http.newRequest("POST", "http://localhost:3000/bar/bat", nil)
+context2 := New().createContext(recorder, req2)
+
+req3, _ := http:New.Request("DELETE", "http://localhost:3000/baz", nil)
+context3 := New().createContext(recorder, req3)
+
