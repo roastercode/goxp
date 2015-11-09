4b1fb8ee934b5813a72496a1e899cfc2e5fcae70
Aurélien Desbrières <aurelien@hackers.camp>
2015-11-09 06:38:45 +0100

    static.go + type StaticOptions

--- static.go
+++ static.go
@@ -9,3 +9,21 @@ import (
 	"string"
 )
 
+// StaticOptions is a struct for specifying configuration options for the goxp.Static middleware.
+type StaticOptions struct {
+	// Prefix is the optional prefix used to serve the static directory content
+	Prefix string
+	// SkipLogging will disable [Static] log messages when a static file is served.
+	SkipLogging bool
+	// IndexFile defines which file to sere as index if it exists.
+	IndexFile string
+	// Expires defines which user-defined function to use for producing a HTTP Expires Header
+	// https://developers.google.com/speed/docs/insights/LeverageBrowserCaching
+	Expires func() string
+	// Fallback defines a default URL to serve when the requested resource was
+	// not found
+	Fallback string
+	// Exclude defines a pattern for URLs this handler should never process.
+	Exclude string
+}
+
